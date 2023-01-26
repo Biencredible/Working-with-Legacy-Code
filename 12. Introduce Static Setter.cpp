b9331d191794f60e3f088e39fc9f4e5ee2dcaebf
Biencredible <fabian.bien@gmx.de>
2023-01-26 08:06:24 +0100

    Introduce Static Setter: Exchanged code example

--- 12. Introduce Static Setter.cpp
+++ 12. Introduce Static Setter.cpp
@@ -18,55 +18,56 @@ Class that should tested, depends on global Singleton.
 
 //=====================================================================================================================
 // Code Example
-// Singleton class in which the router, returned by makeRouter can not be exchanged for testing purposes.
-class RouterFactory
+// Dispatcher is created with Singleton ExternalRouter
+void MessageRouter::route(Message* message)
 {
-    static Router makeRouter()
+    //...
+    Dispatcher* dispatcher = ExternalRouter::instance()->getDispatcher();
+    if(dispatcher != nullptr)
     {
-        return new EWNRouter();
-    }
-};
-
-// Changes to make it possible
-// Create Interface
-class RouterServer
-{
-    Router makeRouter();
-};
-
-// make Router a member, that can be set by setter.
-class RouterFactory
-{
-    RouterServer server;
-
-    static Router makeRouter()
-    {
-        return server.makeRouter()
-    }
-
-    static setServer(RouterServer server)
-    {
-        this->server = server;
-    }
-
-    static RouterServer server = new RouterServer()
-    {
-        public RouterServer makeROuter() 
-        {
-            return new EWNRouter;
-        }
-    };
-};
-
-// In a Test suite the following can be done
-void setUp()
-{
-    RouterServer.setServer(new Routerserver)
-    {
-        public RouterServer makeRouter()
-        {
-            return new FakeRouter();
-        }
+        dispatcher->sendMessage(message);
     }
 }
 
+// ExternalRouter Singleton before any changes
+class ExternalRouter
+{
+private:
+    static ExternalRouter* _instance; // Instance is private
+
+public:
+    static ExternalRouter* instance(); // getter of instance is public
+};
+
+ExternalRouter* ExternalRouter::_instance = 0; // Das hier verstehe ich nicht. Externer Zugriff auf private Member?
+
+ExternalRouter* ExternalRouter::instance()
+{
+    if(_instance == 0)
+    {
+        _instance = new ExternalRouter;
+    }
+    return _instance;
+};
+
+// Return value of instance() has to be changed
+// Implement method to exchange instance
+void ExternlRouter::setTestingInstance(ExternalRouter* newInstance)
+{
+    delete _instance;
+    _instance = newInstance;
+}
+// Change constructor from private to protected.
+// Create a subclass of ExternalRouter for testing purposes
+class TestingExternalRouter : public TestingExternalRouter
+{
+public:
+    virtual void Dispatcher* getDispatcher() const
+    {
+        return new FakeDispatcher;
+    }
+}
+
+/* Now ExternlRouter::setTestingInstance() can be invoked with TestingExternalRouter instance as input parameter.
+Afterwards the call of ExternalRouter::instance()->getDispatcher() in MessageRouter::route() will return the 
+FakeDispatcher from TestingExternalRouter::instance()->getDispatcher().*/
